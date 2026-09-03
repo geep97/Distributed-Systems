65e930055caf3d9883bbdffd875f2b42f7628be9
geep97 <owusugodman@gmail.com>
2026-09-03 13:39:17 +0000

    created the model for ids and queue,and methods

--- api/v1/router/jobs.py
+++ api/v1/router/jobs.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 
 
 router =   APIRouter(tags = ["jobs"])
 
+
+class JobStub(BaseModel):
+    status : str
+    job_id : int
+
+
+
 @router.get(
     "/jobs",)
-
-
-
 
 def jobs(  ):
     return  [{"id":1,"status" :"queued"},
@@ -16,3 +21,20 @@ def jobs(  ):
              {"id":3,"status" :"failed"},]
 
 
+
+@router.get("/jobs/{job_id}")
+def look_jobs(job_id:int):
+    return {
+        "job_id": job_id,
+        "status": "queued",
+
+    }
+
+
+@router.post("/jobs",)
+def create_job(job:JobStub):
+    return {
+        "job_id": job.job_id,
+        "status": job.status
+
+    }
